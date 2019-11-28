65eff918d6d620cb44e8653b03b58e6f11b218f0
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-11-28 14:51:23 +0200

    add tabs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,13 +3,15 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        MatTabsModule
       ],
       declarations: [
         AppComponent,
