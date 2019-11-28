6f9e7c25ba7402cc86541f2203ab15b20a1bdff5
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-11-28 16:25:32 +0200

    app tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { ItemComponent } from './item/item.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MatTabsModule } from '@angular/material/tabs';
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -18,11 +21,19 @@ describe('AppComponent', () => {
         ItemComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+
+    fixture.detectChanges();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('should render component with 24 items and 2 tabs', () => {
+    
+  })
+
 });
