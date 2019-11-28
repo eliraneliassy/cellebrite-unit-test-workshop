173b6de347ebaa427c697b7680a27dd5f34c4f17
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-11-28 12:43:13 +0200

    add tests to master§

--- src/app/master.service.spec.ts
+++ src/app/master.service.spec.ts
@@ -3,10 +3,32 @@ import { TestBed } from '@angular/core/testing';
 import { MasterService } from './master.service';
 
 describe('MasterService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  const consoleMock = {
+    log: () => { }
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+
+      ]
+    });
+    spyOn(window.console, 'log');
+  });
 
   it('should be created', () => {
     const service: MasterService = TestBed.get(MasterService);
     expect(service).toBeTruthy();
+  });
+
+  it('should log the value', () => {
+    const service: MasterService = TestBed.get(MasterService);
+    const val = 'blabla';
+
+    service.logValue(val);
+
+    expect(window.console.log).toHaveBeenCalled();
+
   });
 });
