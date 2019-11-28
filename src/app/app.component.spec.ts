40e2eba4ebe853d6a52c872101e2b205ebc5a56f
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-11-28 10:59:30 +0200

    add http server test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'cellebrite-unit-test-workshop'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cellebrite-unit-test-workshop');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cellebrite-unit-test-workshop app is running!');
-  });
 });
--- src/app/feed.service.spec.ts
+++ src/app/feed.service.spec.ts
@@ -1,12 +1,48 @@
+import { fashionDB } from './fashionDB';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { FeedService } from './feed.service';
 
 describe('FeedService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let service: FeedService;
+  let httpTestingController: HttpTestingController;
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+    service = TestBed.get(FeedService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  }
+  );
 
   it('should be created', () => {
-    const service: FeedService = TestBed.get(FeedService);
     expect(service).toBeTruthy();
   });
+
+  it('should get feed with params', () => {
+    const mockDB = fashionDB;
+
+    service.getFeed(0, 'fashion').subscribe((res) => {
+
+      expect(res).toBeTruthy();
+      expect(res.length).toEqual(mockDB.length);
+      expect(res[0]._id).toEqual(mockDB[0]._id);
+
+    });
+
+    const req = httpTestingController
+      .expectOne('https://api.fashbash.co/api/feed?page=0&categories=fashion');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockDB);
+
+    httpTestingController.verify();
+
+  });
+
 });
