bd523730299594e8582f1103d8fee6ef19fc71fc
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-11-28 15:31:27 +0200

    add item tests

--- src/app/item/item.component.spec.ts
+++ src/app/item/item.component.spec.ts
@@ -1,6 +1,9 @@
+import { fashionDB } from './../fashionDB';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemComponent } from './item.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('ItemComponent', () => {
   let component: ItemComponent;
@@ -8,18 +11,34 @@ describe('ItemComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ItemComponent ]
+      declarations: [ItemComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the item', () => {
+    const item = fashionDB[0];
+
+    component.item = item;
+
+    fixture.detectChanges();
+
+    const title: DebugElement = fixture.debugElement.query(By.css('.title'));
+    expect(title.nativeElement.innerText).toBe(item.description);
+
+    const image: DebugElement = fixture.debugElement.query(By.css('img'));
+    expect(image.nativeElement.src).toBe(item.imageUrl);
+
+    const price: DebugElement = fixture.debugElement.query(By.css('.price'));
+    expect(price.nativeElement.innerText).toBe(item.price);
   });
 });
