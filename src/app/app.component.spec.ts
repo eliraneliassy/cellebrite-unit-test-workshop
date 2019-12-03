a18a037a29aa308c7b7a04948dee5aa529deb0a8
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-12-03 11:52:45 +0200

    first test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,12 +9,15 @@ import { AppComponent } from './app.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
 
   let fixture: ComponentFixture<AppComponent>;
   let app: AppComponent;
   let feedService: FeedService;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     const feedServiceMock = {
@@ -42,12 +45,40 @@ describe('AppComponent', () => {
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.debugElement.componentInstance;
     feedService = TestBed.get(FeedService);
+    el = fixture.debugElement;
 
     fixture.detectChanges();
   }));
 
   it('should create the app', () => {
     expect(app).toBeTruthy();
+  });
+
+  it('should render the component with fashion items and 2 tabs', () => {
+
+    feedService.getFeed(0, 'fashion').subscribe(res => {
+
+      const items = el.queryAll(By.css('.col-3'));
+      const firstItem = items[0];
+      const titleOfFirstItem = firstItem.query(By.css('.title'));
+      expect(titleOfFirstItem.nativeElement.innerText)
+        .toEqual(fashionDB[0].description);
+
+      expect(items.length).toEqual(fashionDB.length);
+
+      const tabs = el.queryAll(By.css('.mat-tab-label'));
+
+      expect(tabs.length).toEqual(2);
+
+    });
+
+  });
+
+  it('should show sports items after clicking the sports tab', () => {
+    
+    
+
+
   });
 
 
